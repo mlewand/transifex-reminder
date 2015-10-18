5be9eabacad12a42e6263bdffa6b2c0c34f7e3c0
m.lewandowski <m.lewandowski@cksource.com>
2015-10-19 01:04:21 +0200

    Added main file.

---
+++ index.js
@@ -0,0 +1,14 @@
+
+'use strict';
+
+let App = require( './src/App.js' ),
+	TransifexApp = require( './src/TransifexApp.js' ),
+	config = require( './config.json' ),
+	pullRequestId = 3,
+	app;
+
+app = new TransifexApp( config );
+app.start();
+
+app.validatePr( pullRequestId );
+
