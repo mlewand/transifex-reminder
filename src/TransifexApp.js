db959514b43a653591a98d965a21fbe56ff6f47e
m.lewandowski <m.lewandowski@cksource.com>
2015-10-19 00:19:09 +0200

    Actually let's switch concept a little bit: let's keep handy dictionary that will map commit hash to file modifications array.

--- src/TransifexApp.js
+++ src/TransifexApp.js
@@ -15,7 +15,7 @@ class TransifexApp extends App {
 		this.getPrCommits( pullRequestId ).then( function( commits ) {
 			let langFileRegExp = /^lang\/.+.js$/,
 				whitelistedLangFiles = [ 'lang/en.js' ],
-				//fileModifications = [].concat.apply( [], commits.map( item => item.files ) ),
+				modifications = {},
 				fileModifications = [].concat.apply( [], commits.map( item => {
 					item.files.srcSha = item.sha;
 					return item.files;
@@ -24,19 +24,36 @@ class TransifexApp extends App {
 					return item.filename && item.filename.match( langFileRegExp ) && whitelistedLangFiles.indexOf( item.filename ) === -1;
 				} );
 
+			// Fill out modifications object in a way, that commit hash is used as a key and contains an array of file modifications
+			// that violates the rule. If commit does not violate it won't be added to object keys.
+			commits.forEach( function( item ) {
+				let violatedFiles = item.files.filter( file => file.filename && file.filename.match( langFileRegExp ) && whitelistedLangFiles.indexOf( file.filename ) === -1 );
+				if ( violatedFiles.length ) {
+					modifications[ item.sha ] = violatedFiles;
+				}
+			} );
+			
 			return {
 				commits: commits,
-				langModifications: langModifications
+				langModifications: langModifications,
+				modifications: modifications
 			};
 		} ).then( ( function( info ) {
-			let langModifications = info.langModifications;
+			let langModifications = info.langModifications,
+				modifications = info.modifications,
+				commits = info.commits;
 			// Now let's put a proper status to the PR, depending if it contains violated files or not.
 			if ( langModifications.length ) {
 				console.log( `PR#${pullRequestId} modifies lang files :(\n\nHere\'s the list: \n* ${langModifications.map( el => el.filename ).join( '\n* ' )}` )
 				// langModifications.forEach( el => this._setCommitStatus( el.sha, 'failure' ) );
+				for ( let sha in modifications ) {
+					console.log( `Failed commit: ${sha}` );
+					this._setCommitStatus( sha, 'failure', 'http://ckeditor.com', `Modified lang files (${modifications[ sha ].map( el => el.filename ).join( ', ' )})` );
+				}
 			} else {
 				console.log( commits[ commits.length - 1 ] );
 				console.log( 'PR looking good, great!' );
+				this._setCommitStatus( commits[ commits.length - 1 ].sha, 'success', null, 'No issues detected' );
 			}
 		} ).bind( this ) ).catch( err => console.log( 'getPrCommits(): ERROR:', err ) );
 	}
@@ -51,7 +68,7 @@ class TransifexApp extends App {
 		return new Promise( ( function( resolve, reject ) {
 			this.repo.status( sha, {
 				state: state,
-				target_url: 'http://foo.dev.null',
+				target_url: url,
 				description: descr || 'Transifex validation failed',
 				context: 'transifex-reminder'
 			}, function( err, data ) {
